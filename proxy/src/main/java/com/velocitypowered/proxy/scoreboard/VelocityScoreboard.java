357769ae6a9759fd836a7e53726175ea4b4afe5f
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-06-17 20:03:10 +0200

    Mark 1.21.6 as supported

--- proxy/src/main/java/com/velocitypowered/proxy/scoreboard/VelocityScoreboard.java
+++ proxy/src/main/java/com/velocitypowered/proxy/scoreboard/VelocityScoreboard.java
@@ -50,7 +50,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class VelocityScoreboard implements ProxyScoreboard {
 
-    public static final ProtocolVersion MAXIMUM_SUPPORTED_VERSION = ProtocolVersion.MINECRAFT_1_21_5;
+    public static final ProtocolVersion MAXIMUM_SUPPORTED_VERSION = ProtocolVersion.MINECRAFT_1_21_6;
 
     @NonNull
     @Getter
